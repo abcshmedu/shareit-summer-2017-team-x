93e0b90b00bafc0b15094f2cf8f60c75b8a21fd2
Christian Keller <christian@fam-kellerclan.de>
2017-04-25 12:54:20 +0200

    Testreperaturen für die Ausführung von Tests durch Maven. Ansonsten kein deployment

--- src/test/java/edu/hm/cs/swa/projekt_2/datamodel/BookTest.java
+++ src/test/java/edu/hm/cs/swa/projekt_2/datamodel/BookTest.java
@@ -1,91 +1,90 @@
 package edu.hm.cs.swa.projekt_2.datamodel;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-
 public class BookTest {
-	
-	final String author = "test дь$`?";
-	final String isbn ="abc123";
-	final String title ="testbook";
 
-	@Test
-	public void testDefaultConstructor(){
-		
-		Book book = new Book();
-		
-		assertNotNull(book);
-		
-		assertEquals(book.getAuthor(),null);
-		assertEquals(book.getIsbn(),null);
-		assertEquals(book.getTitle(),null);
-	}
-	
-	
-	@Test
-	public void testParameterizedConstructor() {
-		
-		
-		Book book = new Book(author, isbn, title);
-		
-		assertEquals(author, book.getAuthor());
-		assertEquals(isbn, book.getIsbn());
-		assertEquals(title, book.getTitle());
-		
-	}
-	
-	@Test
-	public void testSetAuthor() {
-		
-		Book book = new Book();
-		
-		book.setAuthor(author);
-		assertTrue(author.equals(book.getAuthor()));
+    final String author = "test пїЅпїЅ$`?";
+    final String isbn = "abc123";
+    final String title = "testbook";
 
-	}
-	
-	@Test
-	public void testToString(){
-		
-		Book book = new Book(author, isbn, title);
-		
-		book.toString();
-		
-		assertEquals(book.toString(),"Book{Medium{title='testbook'}, author='test дь$`?', isbn='abc123'}");
-		
-	}
-	
-	@Test
-	public void testHashcode(){
-		
-		Book book = new Book(author, isbn, title);
-		
-		// static hash code
-		assertEquals(book.hashCode(), -527491186);
-	}
-	
-	@Test
-	public void testEquals(){
-		
-		Book book = new Book(author, isbn, title);
-		Book book2 = new Book("", isbn, "");
-		
-		assertTrue(book.equals(book));
-		
-		assertFalse(book.equals(new Integer(1)));
-		
-		assertFalse(book.equals(book2));
-		
-		book2.setTitle(title);
-		assertFalse(book.equals(book2));
-		
-		book2.setAuthor(author);
-		assertTrue(book.equals(book2));
-	}
+    @Test
+    public void testDefaultConstructor() {
+
+        Book book = new Book();
+
+        assertNotNull(book);
+
+        assertEquals(book.getAuthor(), null);
+        assertEquals(book.getIsbn(), null);
+        assertEquals(book.getTitle(), null);
+    }
+
+
+    @Test
+    public void testParameterizedConstructor() {
+
+
+        Book book = new Book(author, isbn, title);
+
+        assertEquals(author, book.getAuthor());
+        assertEquals(isbn, book.getIsbn());
+        assertEquals(title, book.getTitle());
+
+    }
+
+    @Test
+    public void testSetAuthor() {
+
+        Book book = new Book();
+
+        book.setAuthor(author);
+        assertTrue(author.equals(book.getAuthor()));
+
+    }
+
+    @Test
+    public void testToString() {
+
+        Book book = new Book(author, isbn, title);
+
+        book.toString();
+
+        assertEquals(book.toString(), "Book{Medium{title='testbook'}, author='test пїЅпїЅ$`?', isbn='abc123'}");
+
+    }
+
+    @Test
+    public void testHashcode() {
+
+        Book book = new Book(author, isbn, title);
+
+        // static hash code
+        assertEquals(947273366, book.hashCode());
+    }
+
+    @Test
+    public void testEquals() {
+
+        Book book = new Book(author, isbn, title);
+        Book book2 = new Book("", isbn, "");
+
+        assertTrue(book.equals(book));
+
+        assertFalse(book.equals(new Integer(1)));
+
+        assertFalse(book.equals(book2));
+
+        book2.setTitle(title);
+        assertFalse(book.equals(book2));
+
+        book2.setAuthor(author);
+        assertTrue(book.equals(book2));
+    }
 
 }
