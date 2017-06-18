a1efb46f6af0b7094bb0e93b585a7168eee4e8ef
Michael Fischer <fische42@hm.edu>
2017-06-18 21:44:34 +0200

    - mocked injector

--- src/test/java/edu/hm/cs/swa/projekt_2/logic/MockTest.java
+++ src/test/java/edu/hm/cs/swa/projekt_2/logic/MockTest.java
@@ -1,40 +1,64 @@
 package edu.hm.cs.swa.projekt_2.logic;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Module;
+import com.google.inject.util.Modules;
+
+import edu.hm.Configuration;
 import edu.hm.cs.swa.projekt_2.datamodel.Book;
 import edu.hm.cs.swa.projekt_2.datamodel.Medium;
 import edu.hm.cs.swa.projekt_2.persistence.DataStore;
 
 public class MockTest {
 
+
 	@Mock
-	private DataStore mockedDataStore;
-	
-	@InjectMocks
-	private MediaServiceImpl mediaService;
+	DataStore mockedDataStore;
 	
 	
 	@Before
 	public void setUp() throws Exception {
+		
+		mockedDataStore = Mockito.mock(DataStore.class);
+		
+		
+		Module testModule = new AbstractModule() {
+			
+			@Override
+			protected void configure() {
+				// TODO Auto-generated method stub
+				
+				 bind(MediaService.class).to(MediaServiceImpl.class);
+
+			        bind(ValidationService.class).toInstance(ValidationServiceImpl.INSTANCE);
+			        
+			        bind(DataStore.class).toInstance(mockedDataStore);
+
+			}
+		};
+		
+		Guice.createInjector(Modules.override(new Configuration()).with(testModule));
+		
 	}
 
 	@Test
-	public void test() {
+    public void getBooks() throws Exception {
 		
-		
-		
-		Medium[] books = {new Book("","",""), new Book("","","")}; 
+		Medium[] books = {new Book("", "", "")};
 		Mockito.when(mockedDataStore.getBooks()).thenReturn(books);
-		
-		Medium[] returnedBooks = mediaService.getBooks();
-		
-		Mockito.verify(mockedDataStore, Mockito.times(1)).getBooks();
-		
-	}
+        
+        MediaService service = new MediaServiceImpl();
+        Medium[] returnedBooks = service.getBooks();
+        
+        Assert.assertTrue(returnedBooks != null);
+        Assert.assertEquals(1, returnedBooks.length);
+    }
 
 }
